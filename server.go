c0f0ad89e242e10fa7e28ba9367187eff6e7de50
Alejandro <acarste@okstate.edu>
2017-11-30 07:08:41 +0000

    initial commit.

---
+++ server.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"blockchain"
+)
+
+func main() {
+	cli := blockchain.Cli{}
+	cli.Run()
+}
---
+++ src/blockchain/ProofOfWork.go
@@ -0,0 +1,80 @@
+package blockchain
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+	"math"
+	"math/big"
+)
+
+var (
+	maxNonce = math.MaxInt64
+)
+
+const TargetBits = 24
+
+type ProofOfWork struct {
+	Block  *Block
+	Target *big.Int
+}
+
+func NewProofOfWork(b *Block) *ProofOfWork {
+	Target := big.NewInt(1)
+	Target.Lsh(Target, uint(256-TargetBits))
+
+	pow := &ProofOfWork{b, Target}
+
+	return pow
+}
+
+func (pow *ProofOfWork) prepareData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.Block.PrevBlockHash,
+			pow.Block.HashTransactions(),
+			IntToHex(pow.Block.Timestamp),
+			IntToHex(int64(TargetBits)),
+			IntToHex(int64(nonce)),
+		},
+		[]byte{},
+	)
+
+	return data
+}
+
+func (pow *ProofOfWork) Run() (int, []byte) {
+	var hashInt big.Int
+	var hash [32]byte
+	nonce := 0
+
+	fmt.Printf("Mining a new block.")
+	for nonce < maxNonce {
+		data := pow.prepareData(nonce)
+
+		hash = sha256.Sum256(data)
+		fmt.Printf("\r%x", hash)
+		hashInt.SetBytes(hash[:])
+
+		if hashInt.Cmp(pow.Target) == -1 {
+			break
+		} else {
+			nonce++
+		}
+	}
+	fmt.Print("\n\n")
+
+	return nonce, hash[:]
+}
+
+func (pow *ProofOfWork) Validate() bool {
+	var hashInt big.Int
+
+	data := pow.prepareData(pow.Block.Nonce)
+	hash := sha256.Sum256(data)
+	hashInt.SetBytes(hash[:])
+
+	isValid := hashInt.Cmp(pow.Target) == -1
+
+	return isValid
+}
